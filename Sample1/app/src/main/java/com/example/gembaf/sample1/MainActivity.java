a49a148bd81296893dd5baca6087bdfc0af4dc1f
gembaf <gembaf.github@gmail.com>
2017-01-30 19:34:52 +0900

    足し算のみできるアプリを作成

--- Sample1/app/src/main/java/com/example/gembaf/sample1/MainActivity.java
+++ Sample1/app/src/main/java/com/example/gembaf/sample1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gembaf.sample1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View view) {
+        TextView result = (TextView) findViewById(R.id.resultTextView); // 結果
+        TextView lhs = (TextView) findViewById(R.id.lhs);  // 左辺
+        TextView rhs = (TextView) findViewById(R.id.rhs);  // 右辺
+
+        int lhsNum = Integer.parseInt(lhs.getText().toString());
+        int rhsNum = Integer.parseInt(rhs.getText().toString());
+        int resultNum = lhsNum + rhsNum;
+
+        result.setText(String.valueOf(resultNum));
     }
 }
