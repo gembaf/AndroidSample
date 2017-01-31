661f8c64d975c57a19ea1ccac10971ff0ab1d932
gembaf <gembaf.github@gmail.com>
2017-01-31 14:28:45 +0900

    四則演算ができるように改良

--- Sample1/app/src/main/java/com/example/gembaf/sample1/MainActivity.java
+++ Sample1/app/src/main/java/com/example/gembaf/sample1/MainActivity.java
@@ -3,25 +3,38 @@ package com.example.gembaf.sample1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView result; // 結果
+    TextView lhs;  // 左辺
+    TextView rhs;  // 右辺
+    RadioGroup operations; // 四則演算の選択肢
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        result = (TextView) findViewById(R.id.resultTextView);
+        lhs = (TextView) findViewById(R.id.lhs);
+        rhs = (TextView) findViewById(R.id.rhs);
+        operations = (RadioGroup) findViewById(R.id.operationsRadioGroup);
     }
 
     public void calculate(View view) {
-        TextView result = (TextView) findViewById(R.id.resultTextView); // 結果
-        TextView lhs = (TextView) findViewById(R.id.lhs);  // 左辺
-        TextView rhs = (TextView) findViewById(R.id.rhs);  // 右辺
-
         int lhsNum = Integer.parseInt(lhs.getText().toString());
         int rhsNum = Integer.parseInt(rhs.getText().toString());
-        int resultNum = lhsNum + rhsNum;
+        int resultNum = 0;
 
+        switch (operations.getCheckedRadioButtonId()) {
+            case R.id.add: resultNum = lhsNum + rhsNum; break;
+            case R.id.sub: resultNum = lhsNum - rhsNum; break;
+            case R.id.mlt: resultNum = lhsNum * rhsNum; break;
+            case R.id.div: resultNum = lhsNum / rhsNum; break;
+        }
         result.setText(String.valueOf(resultNum));
     }
 }
