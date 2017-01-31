cc4bf23b32cdfadc8b13011fd416212aa0364a3a
gembaf <gembaf.github@gmail.com>
2017-01-31 15:57:43 +0900

    sample1に対するテストの追加

--- Sample1/app/src/androidTest/java/com/example/gembaf/sample1/ExampleInstrumentedTest.java
+++ Sample1/app/src/androidTest/java/com/example/gembaf/sample1/ExampleInstrumentedTest.java
@@ -1,12 +1,24 @@
 package com.example.gembaf.sample1;
 
+import static android.support.test.espresso.Espresso.onView;
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +28,44 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
-        assertEquals("com.example.gembaf.sample1", appContext.getPackageName());
+    @Before
+    public void before() {
+        onView(withId(R.id.lhs)).perform(typeText("12"));  // 左辺
+        onView(withId(R.id.rhs)).perform(typeText("3"));   // 右辺
+    }
+
+    // 足し算
+    @Test
+    public void addTest() throws Exception {
+        onView(withId(R.id.add)).perform(click());
+        onView(withId(R.id.myButton)).perform(click());
+        onView(withId(R.id.resultTextView)).check(matches(withText("15")));
+    }
+
+    // 引き算
+    @Test
+    public void subTest() throws Exception {
+        onView(withId(R.id.sub)).perform(click());
+        onView(withId(R.id.myButton)).perform(click());
+        onView(withId(R.id.resultTextView)).check(matches(withText("9")));
+    }
+
+    // 掛け算
+    @Test
+    public void mltTest() throws Exception {
+        onView(withId(R.id.mlt)).perform(click());
+        onView(withId(R.id.myButton)).perform(click());
+        onView(withId(R.id.resultTextView)).check(matches(withText("36")));
+    }
+
+    // 割り算
+    @Test
+    public void divTest() throws Exception {
+        onView(withId(R.id.div)).perform(click());
+        onView(withId(R.id.myButton)).perform(click());
+        onView(withId(R.id.resultTextView)).check(matches(withText("4")));
     }
 }
