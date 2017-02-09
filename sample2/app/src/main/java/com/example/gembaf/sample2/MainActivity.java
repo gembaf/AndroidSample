b5eaf9a1abd8bf8a1d58abb4388c4213d5170ea7
gembaf <gembaf.github@gmail.com>
2017-02-09 16:33:48 +0900

    トップページからの遷移を実装

--- sample2/app/src/main/java/com/example/gembaf/sample2/MainActivity.java
+++ sample2/app/src/main/java/com/example/gembaf/sample2/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.gembaf.sample2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent(getApplication(), SubActivity.class);
+    }
+
+    public void toNextPage(View view) {
+        startActivity(intent);
     }
 }
