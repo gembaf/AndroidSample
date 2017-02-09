baf1c247f78ce1a4e2322e081fb3e2cf6b4f27d5
gembaf <gembaf.github@gmail.com>
2017-02-09 16:37:15 +0900

    遷移先のページからトップページに戻れるようにする

--- sample2/app/src/main/java/com/example/gembaf/sample2/SubActivity.java
+++ sample2/app/src/main/java/com/example/gembaf/sample2/SubActivity.java
@@ -2,6 +2,7 @@ package com.example.gembaf.sample2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+    }
+
+    public void toTopPage(View view) {
+        finish();
     }
 }
