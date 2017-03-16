0b5f747e644afc1c3e27122530dd2ec9f98bde37
gembaf <gembaf.github@gmail.com>
2017-03-16 23:36:49 +0900

    Fragmentを用いてHello Worldを表示する

--- Sample4/app/src/main/java/com/example/gembaf/sample4/MainActivity.java
+++ Sample4/app/src/main/java/com/example/gembaf/sample4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gembaf.sample4;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainFragment fragment = new MainFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, fragment);
+        transaction.commit();
     }
 }
---
+++ Sample4/app/src/main/java/com/example/gembaf/sample4/MainFragment.java
@@ -0,0 +1,26 @@
+package com.example.gembaf.sample4;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gembaf on 2017/03/16.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    // Viewが生成し終わった時に呼ばれるメソッド
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
