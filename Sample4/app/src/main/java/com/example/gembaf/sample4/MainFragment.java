74b93feec738db1b82227ab72c5112470c5461bb
gembaf <gembaf.github@gmail.com>
2017-03-17 01:21:09 +0900

    ボタン押下でJSONをGetする機能を追加

--- Sample4/app/src/main/java/com/example/gembaf/sample4/MainFragment.java
+++ Sample4/app/src/main/java/com/example/gembaf/sample4/MainFragment.java
@@ -1,10 +1,21 @@
 package com.example.gembaf.sample4;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+import static android.R.id.message;
 
 /**
  * Created by gembaf on 2017/03/16.
@@ -12,15 +23,51 @@ import android.view.ViewGroup;
 
 public class MainFragment extends Fragment {
 
+    private TextView textView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
-    // Viewが生成し終わった時に呼ばれるメソッド
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        textView = (TextView) view.findViewById(R.id.textView);
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getQuestion();
+            }
+        });
+    }
+
+    private void getQuestion() {
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String result = null;
+
+                Request request = new Request.Builder()
+                        .url("https://private-7e76b-sample412.apiary-mock.com/questions")
+                        .get()
+                        .build();
+                OkHttpClient client = new OkHttpClient();
+
+                try {
+                    Response response = client.newCall(request).execute();
+                    result = response.body().string();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                textView.setText(result);
+            }
+        }.execute();
     }
 }
