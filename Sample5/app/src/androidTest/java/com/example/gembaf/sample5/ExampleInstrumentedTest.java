4923bd281ef2626219a0406c004a3e6bd3292566
gembaf <gembaf.github@gmail.com>
2017-03-18 22:24:47 +0900

    Sample5 に対してテストを追加

--- Sample5/app/src/androidTest/java/com/example/gembaf/sample5/ExampleInstrumentedTest.java
+++ Sample5/app/src/androidTest/java/com/example/gembaf/sample5/ExampleInstrumentedTest.java
@@ -1,14 +1,23 @@
 package com.example.gembaf.sample5;
 
+import static android.support.test.espresso.Espresso.onView;
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
-
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.containsString;
 /**
  * Instrumentation test, which will execute on an Android device.
  *
@@ -16,11 +25,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.example.gembaf.sample5", appContext.getPackageName());
+        onView(withId(R.id.textView)).check(matches(withText(containsString("Hello"))));
     }
 }
